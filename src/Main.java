abd451954dc5d0989ef3812f21f62fdd8f8122e5
alexkanakis <lakanakis@gmail.com>
2023-03-25 23:06:39 +0200

    Added org.apache.commons.lang3.ArrayUtils to keep the indexes of the monsters, though im not including the library because intelliJ crashes "Analyzing the Code" when I try to push it on GitHub. Here's the link for the library: https://commons.apache.org/proper/commons-lang/download_lang.cgi
    Now monsters can move.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import org.apache.commons.lang3.ArrayUtils;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Monster.buildBattleBoard();
+
+        Monster[] Monsters = new Monster[4];
+
+        Monsters[0] = new Monster(1000, 20, 1, "Vampire");
+        Monsters[1] = new Monster(500, 40, 2, "Slime");
+        Monsters[2] = new Monster(1000, 20, 1, "Skeleton");
+        Monsters[3] = new Monster(1000, 20, 1, "Zombie");
+
+        Monster.redrawBoard();
+
+        for(Monster m : Monsters){
+            if(m.isAlive()){
+                int arrayItemIndex = ArrayUtils.indexOf(Monsters, m);
+                m.moveMonster(Monsters, arrayItemIndex);
+                Monster.redrawBoard();
+            }
+
+        }
+
     }
+
+
 }
